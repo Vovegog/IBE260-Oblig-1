67730750c5efbafe1e04ec9540cae6d75716971b
Vovegog <daniel.viggen@hotmail.com>
2024-02-15 19:07:49 +0100

    Andre forsøk. Ble mye tull på første

--- Oblig 1 - Bridge/main.js
+++ Oblig 1 - Bridge/main.js
@@ -30,57 +30,77 @@ let players = [
 let deck = [];
 // Create a deck and shuffle it
 function createDeck() {
-    deck = [];
-    let suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
-    let ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
-    for (let suit of suits) {
-        for (let rank of ranks) {
-            deck.push(`${rank} of ${suit}`);
+    try {
+        deck = [];
+        let suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
+        let ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+        for (let suit of suits) {
+            for (let rank of ranks) {
+                deck.push(`${rank} of ${suit}`);
+            }
         }
+        // console.log(deck); // Debugging
+        // Shuffle the deck
+        for (let i = deck.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [deck[i], deck[j]] = [deck[j], deck[i]];
+        }
+        // console.log(deck); // Debugging
+        // Assign the cards to the players
+        for (let i = 0; i < deck.length; i++) {
+            players[i % 4][1].push(deck[i]);
+        }
+        console.log(players);
     }
-    // console.log(deck); // Debugging
-    // Shuffle the deck
-    for (let i = deck.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [deck[i], deck[j]] = [deck[j], deck[i]];
+    catch (error) {
+        errorHandler(error);
     }
-    // console.log(deck); // Debugging
-    // Assign the cards to the players
-    for (let i = 0; i < deck.length; i++) {
-        players[i % 4][1].push(deck[i]);
-    }
-    console.log(players);
 }
 // RESTful POST to start the game
 app.post('/start', (req, res) => {
-    if (gameRunning) {
-        res.status(400).send('Game already running');
+    try {
+        if (gameRunning) {
+            res.status(400).send('Game already running');
+        }
+        else {
+            gameRunning = true;
+            createDeck();
+            res.status(200).send('Game started');
+        }
     }
-    else {
-        gameRunning = true;
-        createDeck();
-        res.status(200).send('Game started');
+    catch (error) {
+        errorHandler(error);
     }
 });
 // RESTful POST to restart the game
 app.post('/restart', (req, res) => {
-    gameRunning = false;
-    players = [
-        ['Trond', []],
-        ['Kari', []],
-        ['Per', []],
-        ['Kai Åge', []]
-    ];
-    createDeck();
-    res.status(200).send('Game restarted');
+    try {
+        gameRunning = false;
+        players = [
+            ['Trond', []],
+            ['Kari', []],
+            ['Per', []],
+            ['Kai Åge', []]
+        ];
+        createDeck();
+        res.status(200).send('Game restarted');
+    }
+    catch (error) {
+        errorHandler(error);
+    }
 });
 // RESTful GET to get the players and their hands
 app.get('/players', (req, res) => {
-    if (gameRunning) {
-        res.status(200).json(players);
+    try {
+        if (gameRunning) {
+            res.status(200).json(players);
+        }
+        else {
+            res.status(400).send('Game not running');
+        }
     }
-    else {
-        res.status(400).send('Game not running');
+    catch (error) {
+        errorHandler(error);
     }
 });
 app.listen(8080, () => {
--- Oblig 1 - Bridge/main.ts
+++ Oblig 1 - Bridge/main.ts
@@ -32,61 +32,77 @@ let deck: string[] = [];
 
 // Create a deck and shuffle it
 function createDeck() {
-    deck = [];
-    let suits: string[] = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
-    let ranks: string[] = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
-    for (let suit of suits) {
-        for (let rank of ranks) {
-            deck.push(`${rank} of ${suit}`);
+    try {
+        deck = [];
+        let suits: string[] = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
+        let ranks: string[] = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+        for (let suit of suits) {
+            for (let rank of ranks) {
+                deck.push(`${rank} of ${suit}`);
+            }
         }
-    }
-    // console.log(deck); // Debugging
+        // console.log(deck); // Debugging
 
-    // Shuffle the deck
-    for (let i: number = deck.length - 1; i > 0; i--) {
-        let j: number = Math.floor(Math.random() * (i + 1));
-        [deck[i], deck[j]] = [deck[j], deck[i]];
-    }
-    // console.log(deck); // Debugging
+        // Shuffle the deck
+        for (let i: number = deck.length - 1; i > 0; i--) {
+            let j: number = Math.floor(Math.random() * (i + 1));
+            [deck[i], deck[j]] = [deck[j], deck[i]];
+        }
+        // console.log(deck); // Debugging
 
-    // Assign the cards to the players
-    for (let i: number = 0; i < deck.length; i++) {
-        players[i % 4][1].push(deck[i]);
+        // Assign the cards to the players
+        for (let i: number = 0; i < deck.length; i++) {
+            players[i % 4][1].push(deck[i]);
+        }
+        console.log(players);
+    } catch (error) {
+        errorHandler(error);
     }
-    console.log(players);
 }
 
 // RESTful POST to start the game
 app.post('/start', (req: Request, res: Response) => {
-    if (gameRunning) {
-        res.status(400).send('Game already running');
-    } else {
-        gameRunning = true;
-        createDeck();
-        res.status(200).send('Game started');
+    try {
+        if (gameRunning) {
+            res.status(400).send('Game already running');
+        } else {
+            gameRunning = true;
+            createDeck();
+            res.status(200).send('Game started');
+        }
+    } catch (error) {
+        errorHandler(error);
     }
 });
 
 // RESTful POST to restart the game
 app.post('/restart', (req: Request, res: Response) => {
-    gameRunning = false;
-    players = [
-        ['Trond', []],
-        ['Kari', []],
-        ['Per', []],
-        ['Kai Åge', []]
-    ];
-    createDeck();
-    res.status(200).send('Game restarted');
+    try {
+        gameRunning = false;
+        players = [
+            ['Trond', []],
+            ['Kari', []],
+            ['Per', []],
+            ['Kai Åge', []]
+        ];
+        createDeck();
+        res.status(200).send('Game restarted');
+    } catch (error) {
+        errorHandler(error);
+    }
 });
 
 
 // RESTful GET to get the players and their hands
 app.get('/players', (req: Request, res: Response) => {
-    if (gameRunning) {
-        res.status(200).json(players);
-    } else {
-        res.status(400).send('Game not running');
+    try {
+        if (gameRunning) {
+            res.status(200).json(players);
+        } else {
+            res.status(400).send('Game not running');
+        }
+    } catch (error) {
+        errorHandler(error);
     }
 });
 
