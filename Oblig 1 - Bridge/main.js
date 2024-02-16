cdedb019bd97a2cc1c1642456fbc0134cb8bd6d8
Vovegog <daniel.viggen@hotmail.com>
2024-02-16 13:00:22 +0100

    Compiled from main.ts

--- Oblig 1 - Bridge/main.js
+++ Oblig 1 - Bridge/main.js
@@ -28,7 +28,7 @@ function errorHandler(err) {
 }
 // Boolean to check if the game is running
 let gameRunning = false;
-// Create an array holding the 4 players with their hands
+// Create an array holding the 4 players with their hands (of cards)
 let players = [
     ['Trond', []],
     ['Kari', []],
@@ -41,7 +41,7 @@ let team2 = [players[1][0], players[3][0]];
 /* Can theoretically add a 3rd index representing a numerical score for the team */
 // Initiate variable "deck"
 let deck = [];
-// Declare a variable for bidding
+// Declare variables for bidding and who put the bid
 let bidding = 0;
 let whoBid = '';
 // Variable for storing passes in a row. 3 PASSES IN A ROW ENDS THE ROUND!!!
@@ -53,7 +53,7 @@ let turn = players[0][0];
 // Create a deck and shuffle it
 function createDeck() {
     try {
-        deck = [];
+        deck = []; // Important to reset the deck before creating a new one in case of restart of the game
         let suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
         let ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
         for (let suit of suits) {
@@ -62,7 +62,7 @@ function createDeck() {
             }
         }
         // console.log(deck); // Debugging
-        // Shuffle the deck
+        // Shuffle the deck. I honestly can't tell you how this works, it's a shuffle algorithm I found online
         for (let i = deck.length - 1; i > 0; i--) {
             let j = Math.floor(Math.random() * (i + 1));
             [deck[i], deck[j]] = [deck[j], deck[i]];
@@ -70,9 +70,9 @@ function createDeck() {
         // console.log(deck); // Debugging
         // Assign the cards to the players
         for (let i = 0; i < deck.length; i++) {
-            players[i % 4][1].push(deck[i]);
-        }
-        console.log(players);
+            players[i % 4][1].push(deck[i]); // Assigns one card to each player, then loops back to the first player
+        } // Could be done with a slice of the deck, but whatever. It works and it's not a lot of code
+        // console.log(players); // Debugging
     }
     catch (error) {
         errorHandler(error);
@@ -87,8 +87,10 @@ app.post('/start', (req, res) => {
         else {
             gameRunning = true;
             createDeck();
-            res.status(200).send('Game started');
-            console.log(`It is now ${turn}'s turn to bid`);
+            /* Res.status sends out the message to display in the client, whose turn it is
+            and the players hands to be displayed in the client, when we get to that in assignment 2 */
+            res.status(200).json({ message: `Game started. It is now ${turn}'s turn to bid`, turn: turn, players: players });
+            // console.log(`It is now ${turn}'s turn to bid`); // Debugging
         }
     }
     catch (error) {
@@ -106,23 +108,24 @@ app.post('/restart', (req, res) => __awaiter(void 0, void 0, void 0, function* (
         ];
         createDeck();
         // Reset the game variables
-        round = 0;
+        round = 1;
         turn = players[0][0];
         bidding = 0;
         passes = 0;
-        res.status(200).send('Game restarted');
-        console.log(`It is now ${turn}'s turn to bid`);
+        // Res.status sends out the message to display in the client, and whose turn it is
+        res.status(200).json({ message: `Game restarted. It is now ${turn}'s turn to bid`, turn: turn });
+        // console.log(`It is now ${turn}'s turn to bid`); // Debugging
     }
     catch (error) {
         errorHandler(error);
     }
 }));
-// RESTful GET to get the players and their hands
+// RESTful GET to get the players and their hands. Mostly for debugging purposes, but could be used for the players to see their hands
 app.get('/players', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         if (gameRunning) {
-            res.status(200).json(players);
-            console.log(`It is currently ${turn}'s turn to bid`);
+            res.status(200).json({ message: `Game is running. It is currently ${turn}'s turn to bid`, players: players });
+            // console.log(`It is currently ${turn}'s turn to bid`); // Debugging
         }
         else {
             res.status(400).send('Game not running');
@@ -149,52 +152,63 @@ app.get('/teams', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
 // RESTful POST to make a bid
 app.post('/bid', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     // Check if player is allowed to bid
-    if (req.body.player !== turn) {
-        res.status(400).send(`It is not your turn to bid. It is ${turn}'s turn to bid`);
-        console.log(`It is not ${req.body.player}'s turn to bid, it is ${turn}'s turn to bid`);
-        return;
-    }
-    else {
-        // If player bids "pass"
-        if (req.body.bid === "PASS") {
-            passes++;
-            // Console out that the player passed, and who's next up to bid
-            console.log(`${turn} passed. It is now ${players[(players.findIndex(player => player[0] === turn) + 1) % 4][0]}'s turn to bid`);
-            // Pass the turn to the next player
-            turn = players[(players.findIndex(player => player[0] === turn) + 1) % 4][0];
-            // If passes is 3, the round is over
-            if (passes === 3) {
-                console.log(`Round ${round} is over`);
-                round++;
-                // Reset passes to 0 for next round
+    try {
+        if (req.body.player !== turn) {
+            res.status(400).send(`It is not your turn to bid. It is ${turn}'s turn to bid`);
+            // console.log(`It is not ${req.body.player}'s turn to bid, it is ${turn}'s turn to bid`) // Debugging
+            return;
+        }
+        else {
+            // If player bids "pass"
+            if (req.body.bid === "PASS") {
+                passes++;
+                // Check passes. If passes in a row is 3, the round is over
+                if (passes === 3) {
+                    // console.log(`Round ${round} is over`); // Debugging
+                    round++;
+                    // Reset passes to 0 for next round
+                    passes = 0;
+                    // Reset the bidding to 0 for next round
+                    bidding = 0;
+                    // Reset the turn to player 1 for next round
+                    turn = players[0][0];
+                    // Reset the players hands for next round *NOT WORKING YET*
+                    res.status(200).json({ message: `Round ${round - 1} is over. It is now ${turn}'s turn to bid`, turn: turn, round: round });
+                    // console.log(`Game is now in round number ${round}`); // Debugging
+                    // If round ends: Exit the function
+                    return;
+                }
+                // Instead of console.log, we use res.status().json() to send the message to the client
+                res.status(200).json({ message: `${turn} passed. It is now ${players[(players.findIndex(player => player[0] === turn) + 1) % 4][0]}'s turn to bid`, turn: turn });
+                // console.log(`${turn} passed. It is now ${players[(players.findIndex(player => player[0] === turn) + 1) % 4][0]}'s turn to bid`); // Debugging
+                // Pass the turn to the next player
+                turn = players[(players.findIndex(player => player[0] === turn) + 1) % 4][0];
+            }
+            else if (req.body.bid <= 0) { // If player bids a 0 or less than 0 (which is impossible)
+                res.status(400).send(`You cannot bid 0 or less than the current bid, ${turn}`);
+                return;
+            }
+            else if (req.body.bid < bidding) { // If player bids lower than the current bid (which is impossible)
+                res.status(400).send(`You cannot bid lower than the current bid, ${turn}`);
+                return;
+            }
+            else { // If player bids a number higher than the current bid
+                bidding = req.body.bid;
+                whoBid = turn;
                 passes = 0;
-                // Reset the bidding to 0 for next round
-                bidding = 0;
-                // Reset the turn to player 1 for next round
-                turn = players[0][0];
-                // Reset the players hands for next round *NOT WORKING YET*
-                console.log(`Game is now in round number ${round}`);
+                // Pass the turn to the next player
+                turn = players[(players.findIndex(player => player[0] === turn) + 1) % 4][0];
+                res.status(200).json({ message: `The current bid is ${bidding} by ${whoBid}. It is now ${turn}'s turn to bid`, turn: turn, bidding: bidding, whoBid: whoBid });
+                // console.log(`The current bid is ${bidding} by ${whoBid}. It is now ${turn}'s turn to bid`); // Debugging
             }
         }
-        else if (req.body.bid <= 0) { // If player bids a 0 or less than 0 (which is impossible)
-            res.status(400).send(`You cannot bid 0 or less than the current bid, ${turn}`);
-            return;
-        }
-        else if (req.body.bid < bidding) { // If player bids lower than the current bid (which is impossible)
-            res.status(400).send(`You cannot bid lower than the current bid, ${turn}`);
-            return;
-        }
-        else { // If player bids a number higher than the current bid
-            bidding = req.body.bid;
-            whoBid = turn;
-            passes = 0;
-            // Pass the turn to the next player
-            turn = players[(players.findIndex(player => player[0] === turn) + 1) % 4][0];
-            // Console out which players turn is next, as well as the current bid
-            console.log(`The current bid is ${bidding} by ${whoBid}. It is now ${turn}'s turn to bid`);
-        }
+    }
+    catch (error) {
+        errorHandler(error);
     }
 }));
 app.listen(80, () => {
     console.log('Server started. Fuck you.');
 });
+/* I MAY want to change all the console.log()'s over to res.status().send's instead?
+Not sure what the difference is tbh. Will have to look it up */ 
