034be18ab9c8a84645e40bac1171a2d21af0cc89
Vovegog <daniel.viggen@hotmail.com>
2024-02-16 12:59:11 +0100

    Did some more commenting, formatted all the res.status().json's and res.status().send's that I (think) will be needed. The project *should* be ready for a client side now

--- Oblig 1 - Bridge/main.ts
+++ Oblig 1 - Bridge/main.ts
@@ -19,7 +19,7 @@ function errorHandler (err: any) {
 // Boolean to check if the game is running
 let gameRunning: boolean = false;
 
-// Create an array holding the 4 players with their hands
+// Create an array holding the 4 players with their hands (of cards)
 let players: [string, string[]][] = [
     ['Trond', []],
     ['Kari', []],
@@ -35,7 +35,7 @@ let team2: string[] = [players[1][0], players[3][0]];
 // Initiate variable "deck"
 let deck: string[] = [];
 
-// Declare a variable for bidding
+// Declare variables for bidding and who put the bid
 let bidding: number = 0;
 let whoBid: string = '';
 
@@ -51,7 +51,7 @@ let turn: string = players[0][0];
 // Create a deck and shuffle it
 function createDeck() {
     try {
-        deck = [];
+        deck = []; // Important to reset the deck before creating a new one in case of restart of the game
         let suits: string[] = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
         let ranks: string[] = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
         for (let suit of suits) {
@@ -61,7 +61,7 @@ function createDeck() {
         }
         // console.log(deck); // Debugging
 
-        // Shuffle the deck
+        // Shuffle the deck. I honestly can't tell you how this works, it's a shuffle algorithm I found online
         for (let i: number = deck.length - 1; i > 0; i--) {
             let j: number = Math.floor(Math.random() * (i + 1));
             [deck[i], deck[j]] = [deck[j], deck[i]];
@@ -70,9 +70,9 @@ function createDeck() {
 
         // Assign the cards to the players
         for (let i: number = 0; i < deck.length; i++) {
-            players[i % 4][1].push(deck[i]);
-        }
-        console.log(players);
+            players[i % 4][1].push(deck[i]); // Assigns one card to each player, then loops back to the first player
+        }                                    // Could be done with a slice of the deck, but whatever. It works and it's not a lot of code
+        // console.log(players); // Debugging
     } catch (error) {
         errorHandler(error);
     }
@@ -86,8 +86,10 @@ app.post('/start', (req: Request, res: Response) => {
         } else {
             gameRunning = true;
             createDeck();
-            res.status(200).send('Game started');
-            console.log(`It is now ${turn}'s turn to bid`);
+            /* Res.status sends out the message to display in the client, whose turn it is
+            and the players hands to be displayed in the client, when we get to that in assignment 2 */
+            res.status(200).json( { message: `Game started. It is now ${turn}'s turn to bid`, turn: turn, players: players } );
+            // console.log(`It is now ${turn}'s turn to bid`); // Debugging
         }
     } catch (error) {
         errorHandler(error);
@@ -105,24 +107,25 @@ app.post('/restart', async (req: Request, res: Response) => {
         ];
         createDeck();
         // Reset the game variables
-        round = 0;
+        round = 1;
         turn = players[0][0];
         bidding = 0;
         passes = 0;
-        res.status(200).send('Game restarted');
-        console.log(`It is now ${turn}'s turn to bid`);
+        // Res.status sends out the message to display in the client, and whose turn it is
+        res.status(200).json( { message: `Game restarted. It is now ${turn}'s turn to bid`, turn: turn } );
+        // console.log(`It is now ${turn}'s turn to bid`); // Debugging
     } catch (error) {
         errorHandler(error);
     }
 });
 
 
-// RESTful GET to get the players and their hands
+// RESTful GET to get the players and their hands. Mostly for debugging purposes, but could be used for the players to see their hands
 app.get('/players', async (req: Request, res: Response) => {
     try {
         if (gameRunning) {
-            res.status(200).json(players);
-            console.log(`It is currently ${turn}'s turn to bid`);
+            res.status(200).json( { message: `Game is running. It is currently ${turn}'s turn to bid`, players: players } );
+            // console.log(`It is currently ${turn}'s turn to bid`); // Debugging
         } else {
             res.status(400).send('Game not running');
         }
@@ -150,19 +153,15 @@ app.post('/bid', async (req: Request, res: Response) => {
     try {
         if (req.body.player !== turn) {
             res.status(400).send(`It is not your turn to bid. It is ${turn}'s turn to bid`);
-            console.log(`It is not ${req.body.player}'s turn to bid, it is ${turn}'s turn to bid`)
+            // console.log(`It is not ${req.body.player}'s turn to bid, it is ${turn}'s turn to bid`) // Debugging
             return;
         } else { 
             // If player bids "pass"
             if (req.body.bid === "PASS") {
                 passes++;
-                // Console out that the player passed, and who's next up to bid
-                console.log(`${turn} passed. It is now ${players[(players.findIndex(player => player[0] === turn) + 1) % 4][0]}'s turn to bid`);
-                // Pass the turn to the next player
-                turn = players[(players.findIndex(player => player[0] === turn) + 1) % 4][0];
-                // If passes is 3, the round is over
+                // Check passes. If passes in a row is 3, the round is over
                 if (passes === 3) {
-                    console.log(`Round ${round} is over`);
+                    // console.log(`Round ${round} is over`); // Debugging
                     round++;
                     // Reset passes to 0 for next round
                     passes = 0;
@@ -171,8 +170,19 @@ app.post('/bid', async (req: Request, res: Response) => {
                     // Reset the turn to player 1 for next round
                     turn = players[0][0];
                     // Reset the players hands for next round *NOT WORKING YET*
-                    console.log(`Game is now in round number ${round}`);
+                    res.status(200).json( { message: `Round ${round-1} is over. It is now ${turn}'s turn to bid`, turn: turn, round: round } );
+                    // console.log(`Game is now in round number ${round}`); // Debugging
+                    // If round ends: Exit the function
+                    return;
                 }
+
+            // Instead of console.log, we use res.status().json() to send the message to the client
+            res.status(200).json( { message: `${turn} passed. It is now ${players[(players.findIndex(player => player[0] === turn) + 1) % 4][0]}'s turn to bid`, turn: turn } );
+            // console.log(`${turn} passed. It is now ${players[(players.findIndex(player => player[0] === turn) + 1) % 4][0]}'s turn to bid`); // Debugging
+
+            // Pass the turn to the next player
+            turn = players[(players.findIndex(player => player[0] === turn) + 1) % 4][0];
+
             } else if (req.body.bid <= 0 ) { // If player bids a 0 or less than 0 (which is impossible)
                 res.status(400).send(`You cannot bid 0 or less than the current bid, ${turn}`);
                 return;
@@ -185,8 +195,8 @@ app.post('/bid', async (req: Request, res: Response) => {
                 passes = 0;
                 // Pass the turn to the next player
                 turn = players[(players.findIndex(player => player[0] === turn) + 1) % 4][0];
-                // Console out which players turn is next, as well as the current bid
-                console.log(`The current bid is ${bidding} by ${whoBid}. It is now ${turn}'s turn to bid`);
+                res.status(200).json( { message: `The current bid is ${bidding} by ${whoBid}. It is now ${turn}'s turn to bid`, turn: turn, bidding: bidding, whoBid: whoBid } );
+                // console.log(`The current bid is ${bidding} by ${whoBid}. It is now ${turn}'s turn to bid`); // Debugging
             }
         } 
     } catch (error) {
