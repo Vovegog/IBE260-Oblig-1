59b1dc00607d45d7c856e43ffcc2f198b0b9315d
Vovegog <daniel.viggen@hotmail.com>
2024-02-29 19:26:25 +0100

    created types Rank and Suit

--- Oblig 1 - Bridge/src/bridgeRules.ts
+++ Oblig 1 - Bridge/src/bridgeRules.ts
@@ -3,18 +3,20 @@ import { playedCard, rank, errorHandler, bidding } from "./main";
 
 const rankDictionary = { "2": 1, "3": 2, "4": 3, "5": 4, "6": 5, "7": 6, "8": 7, "9": 8, "10": 9, "Jack": 10, "Queen": 11, "King": 12, "Ace": 13 }; // Dictionary to convert the rank of the card to a numbered value
 const suitDictionary = { "Clubs": 1, "Diamonds": 2, "Hearts": 3, "Spades": 4 }; // Dictionary to convert the suit of the card to a number, in ascending order of value
+type Suit = keyof typeof suitDictionary;
+type Rank = keyof typeof rankDictionary;
 
 export function checkValidBid(playedcard: [string, rank]): boolean {
     try {
         let valid = false;
 
         // We need to check if the played card has a value higher than the current bid (bidding). If it doesn't, return false
-        const playedCardSuit: keyof typeof suitDictionary = playedcard[0] as keyof typeof suitDictionary;   // Convert the suit of the played card to a number
-        const playedCardValue: keyof typeof rankDictionary = playedcard[1] as keyof typeof rankDictionary;  // Convert the rank of the played card to a number
+        const playedCardSuit: Suit = playedcard[0] as Suit;   // Convert the suit of the played card to a number
+        const playedCardValue: Rank = playedcard[1] as Rank;  // Convert the rank of the played card to a number
         
         // Now we convert the current bid to numbers as well, for easy comparison later
-        const currentBidSuit: keyof typeof suitDictionary = bidding[0] as keyof typeof suitDictionary;   // Convert the suit of the current bid to a number
-        const currentBidValue: keyof typeof rankDictionary = bidding[1] as keyof typeof rankDictionary;  // Convert the rank of the current bid to a number
+        const currentBidSuit: Suit = bidding[0] as Suit;   // Convert the suit of the current bid to a number
+        const currentBidValue: Rank = bidding[1] as Rank;  // Convert the rank of the current bid to a number
         
         // Now we can compare the played card to the current bid
         if (rankDictionary[playedCardValue] > rankDictionary["8"]) { // If the value of the played card is higher than 7, it's not valid
