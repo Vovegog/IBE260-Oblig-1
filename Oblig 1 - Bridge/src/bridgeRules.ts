cb77379587da13f211536032fa39386c61ae2a1d
Vovegog <daniel.viggen@hotmail.com>
2024-02-29 21:11:41 +0100

    Done. Not touching it anymore

--- Oblig 1 - Bridge/src/bridgeRules.ts
+++ Oblig 1 - Bridge/src/bridgeRules.ts
@@ -1,18 +1,20 @@
 // This is where we will write all our bridge rules/logic
-import { playedCard, rank, errorHandler, bidding } from "./main";
+import { rank, errorHandler, bidding } from "./main";
 
 const rankDictionary = { "2": 1, "3": 2, "4": 3, "5": 4, "6": 5, "7": 6, "8": 7, "9": 8, "10": 9, "Jack": 10, "Queen": 11, "King": 12, "Ace": 13 }; // Dictionary to convert the rank of the card to a numbered value
 const suitDictionary = { "Clubs": 1, "Diamonds": 2, "Hearts": 3, "Spades": 4 }; // Dictionary to convert the suit of the card to a number, in ascending order of value
 type Suit = keyof typeof suitDictionary;
 type Rank = keyof typeof rankDictionary;
 
-export function checkValidBid(playedcard: [string, rank]): boolean {
+export function checkValidBid(cardToCheck: [string, rank]): boolean {
     try {
         let valid = false;
 
+        
+
         // We need to check if the played card has a value higher than the current bid (bidding). If it doesn't, return false
-        const playedCardSuit: Suit = playedcard[0] as Suit;   // Convert the suit of the played card to a number
-        const playedCardValue: Rank = playedcard[1] as Rank;  // Convert the rank of the played card to a number
+        const playedCardSuit: Suit = cardToCheck[0] as Suit;   // Convert the suit of the played card to a number
+        const playedCardValue: Rank = cardToCheck[1] as Rank;  // Convert the rank of the played card to a number
         
         // Now we convert the current bid to numbers as well, for easy comparison later
         const currentBidSuit: Suit = bidding[0] as Suit;   // Convert the suit of the current bid to a number
